d27c831c84c8004d0d407511205cd15d277b6969
BonelessFrogs <320275@my.puhsd.org>
2023-03-21 16:57:59 -0700

    Renamed strafing encoders for convenience

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -74,7 +74,7 @@ public class Auto extends LinearOpMode {
         encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
         encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
-        
+
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
@@ -156,7 +156,7 @@ public class Auto extends LinearOpMode {
         }
     }
     //strafing encoders
-    public void strafing(double speed, double leftInches, double rightInches, double timeoutS){
+    public void encoderStrafe(double speed, double leftInches, double rightInches, double timeoutS){
 
         int newflTarget;
         int newfrTarget;
