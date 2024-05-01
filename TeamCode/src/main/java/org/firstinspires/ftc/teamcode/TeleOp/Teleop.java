0eb1a65546043a2b9bde95b62469ebc81f114f2e
Unnece55ary5face <166060662+Unnece55ary5face@users.noreply.github.com>
2024-04-30 17:29:53 -0700

    Update Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Teleop.java
@@ -7,15 +7,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Teleop")
 public class Teleop extends OpMode {
+
     DcMotor fL;
     DcMotor bL;
     DcMotor fR;
     DcMotor bR;
-    DcMotor Lift;
-    DcMotor wrist;
-    Servo LS;
-    Servo RS;
-    Servo BT;
+    DcMotor viper;
+    CRServo inTake;
     CRServo rotate;
 
 
@@ -25,19 +23,15 @@ public class Teleop extends OpMode {
         bL = hardwareMap.dcMotor.get("bL");
         fR = hardwareMap.dcMotor.get("fR");
         bR = hardwareMap.dcMotor.get("bR");
-        Lift = hardwareMap.dcMotor.get("Lift");
-        wrist = hardwareMap.dcMotor.get("wrist");
-        LS = hardwareMap.servo.get("LS");
-        RS = hardwareMap.servo.get("RS");
-        BT = hardwareMap.servo.get("BT");
+        viper = hardwareMap.dcMotor.get("viper");
+        inTake = hardwareMap.crservo.get("inTake");
         rotate = hardwareMap.crservo.get("rotate");
-
 
         fL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         fR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         bL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         bR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        Lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        viper.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
     }
@@ -46,8 +40,8 @@ public class Teleop extends OpMode {
     public void loop() {
         //Front back Left
         if (Math.abs(gamepad1.left_stick_y) > .2) {
-            fL.setPower(-gamepad1.left_stick_y * -1);
-            bL.setPower(-gamepad1.left_stick_y * -1);
+            fL.setPower(-gamepad1.left_stick_y * 1);
+            bL.setPower(-gamepad1.left_stick_y * 1);
         } else {
             fL.setPower(0);
             bL.setPower(0);
@@ -67,7 +61,8 @@ public class Teleop extends OpMode {
             bL.setPower(-.9);
             fR.setPower(-.9);
             bR.setPower(.9);
-        } else {
+        }
+        else {
             fL.setPower(0);
             bL.setPower(0);
             fR.setPower(0);
@@ -79,7 +74,8 @@ public class Teleop extends OpMode {
             bL.setPower(.9);
             fR.setPower(.9);
             bR.setPower(-.9);
-        } else {
+        }
+        else {
             fL.setPower(0);
             bL.setPower(0);
             fR.setPower(0);
@@ -87,49 +83,33 @@ public class Teleop extends OpMode {
         }
         //Cascade
         if (Math.abs(-gamepad2.left_stick_y) > .1) {
-            Lift.setPower(gamepad2.left_stick_y * -.5);
-        } else {
-            Lift.setPower(0);
+            viper.setPower(gamepad2.left_stick_y * .5);
         }
-        if (Math.abs(-gamepad2.right_stick_y) > .1) {
-            wrist.setPower(gamepad2.right_stick_y * .5);
-        } else {
-            wrist.setPower(0);
+        else {
+            viper.setPower(0);
         }
         //Intake in
         if (gamepad2.b) {
-            LS.setPosition(.76);
-            RS.setPosition(.20);
+            inTake.setPower(1);
         }
-
         //Intake out
         else if (gamepad2.a) {
-            LS.setPosition(.90);
-            RS.setPosition(0.05);
+            inTake.setPower(-1);
         }
         //Rotate Left
         if (gamepad2.left_bumper) {
-            rotate.setPower(-1);
-        } else {
+            rotate.setPower(-.9);
+        }
+        else {
             rotate.setPower(0);
         }
         //Rotate Right
         if (gamepad2.right_bumper) {
-            rotate.setPower(2);
-        } else {
+            rotate.setPower(.9);
+        }
+        else {
             rotate.setPower(0);
         }
-        //Reset servos position
-        if (gamepad2.dpad_left) {
-            LS.setPosition(1);
-            RS.setPosition(0);
-            BT.setPosition(1);
-        }
-        if (gamepad2.dpad_up) {
-            BT.setPosition(.60);
-        }
-        if (gamepad2.dpad_down) {
-            BT.setPosition(1);
-        }
+
     }
 }
